ba6fab1bbc822b21300d3c1ac43b3ea699669eca
nfabsikova <nfabsikova@gmail.com>
2021-03-29 14:28:17 +0200

    Day 1

---
+++ lesson2/pseudocode.js
@@ -0,0 +1,54 @@
+// a function that returns the sum of two numbers
+
+/*
+START
+  GET number1
+  GET number2
+  RETURN number1 + number2
+END
+*/
+
+//a function that takes an array of strings, and returns a string that is all those strings concatenated together
+
+/*
+START
+  GET array
+  RETURN array.join()
+END
+*/
+
+//a function that takes an array of integers, and returns a new array with every other element
+
+/*
+Casual:
+
+Given an array of integers.
+
+Initialise a new empty array.
+
+Iterate through the input array with a step of two.
+  - in each iteration, push the value at current index into the new array.
+
+Return the new array.
+
+Formal:
+
+START
+
+# Given a collection of integers, called "numbers"
+
+SET newArray = []
+FOR iterator = 1 UNTIL iterator < length of numbers BY iterator += 2
+  newArray.push(numbers[iterator])
+RETURN newArray
+
+END
+
+*/
+
+
+
+
+
+
+
