4b602bcac2ac55cd512c6a5f0d501c13ae4b9a1e
nfabsikova <nfabsikova@gmail.com>
2021-04-05 00:44:13 +0200

    rps bonuses

--- lesson2/rock_paper_scissors.js
+++ lesson2/rock_paper_scissors.js
@@ -1,47 +1,160 @@
 const readline = require('readline-sync');
-const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+const REQUIRED_NUM_WINS = 5;
+const CHOICES = {
+  rock : {shorthand : 'r', beats : ['scissors', 'lizard']},
+  scissors : {shorthand : 's', beats : ['paper', 'lizard']},
+  paper : {shorthand : 'p', beats : ['rock', 'spock']},
+  lizard : {shorthand : 'l', beats : ['paper', 'spock']},
+  spock : {shorthand : 'sp', beats : ['rock', 'scissors']}
+};
+
+let scores;
 
 function prompt(message) {
-  console.log(`=> ${message}`);
+  console.log(`> ${message}`);
 }
 
-function displayWinner(choice, computerChoice) {
+function input() {
+  return readline.question("==> ");
+}
+
+function formatResult(message) {
+  return `\n*** ${message} ***\n`;
+}
+
+function consoleDivider() {
+  console.log('-----------------------------------------------------');
+}
+
+function choicesAsString() {
+  let choiceArray = [];
+  for (let key in CHOICES) {
+    choiceArray.push(key + " (" + CHOICES[key].shorthand + ")");
+  }
+  return choiceArray.join(", ");
+}
+
+function shorthandsAsString() {
+  let shorthandArray = [];
+  for (let key in CHOICES) {
+    shorthandArray.push("'" + CHOICES[key].shorthand + "'");
+  }
+  return shorthandArray.join(", ");
+}
+function displayWelcome() {
+  console.clear();
+  prompt(`Welcome to ${Object.keys(CHOICES).join(', ')}!`);
+  consoleDivider();
+  prompt("Whoever wins five rounds becomes the grand winner.");
+}
+
+function promptContinue() {
+  prompt("Press enter to continue.");
+  readline.question();
+  console.clear();
+}
+
+function displayScores() {
+  prompt(`SCORE: Player: ${scores.player}, Computer: ${scores.computer}`);
+  consoleDivider();
+}
+
+function assignChoice(letter) {
+  for (let key in CHOICES) {
+    if (CHOICES[key].shorthand === letter) return key;
+  }
+  return 'invalid';
+}
+
+function getPlayerChoice() {
+  prompt(`Choose one: ${choicesAsString()}`);
+  let choiceShorthand = input().toLowerCase();
+  let choice = assignChoice(choiceShorthand);
+
+  while (choice === 'invalid') {
+    prompt(`That's not a valid choice. Please enter one of: ${shorthandsAsString()}`);
+    choiceShorthand = input().toLowerCase();
+    choice = assignChoice(choiceShorthand);
+  }
+  return choice;
+}
+
+function returnComputerChoice() {
+  let randomIndex = Math.floor(Math.random() * Object.keys(CHOICES).length);
+  return Object.keys(CHOICES)[randomIndex];
+}
+
+function returnWinner(choice, computerChoice) {
+  if (choice === computerChoice) {
+    return 'tie';
+  } else if (CHOICES[choice]['beats'].includes(computerChoice)) {
+    return 'player';
+  } else {
+    return 'computer';
+  }
+}
+
+function displayRoundResult(choice, computerChoice, winner) {
   prompt(`You chose ${choice}, computer chose ${computerChoice}`);
 
-  if ((choice === 'rock' && computerChoice === 'scissors') ||
-      (choice === 'paper' && computerChoice === 'rock') ||
-      (choice === 'scissors' && computerChoice === 'paper')) {
-    prompt('You win!');
-  } else if ((choice === 'rock' && computerChoice === 'paper') ||
-              (choice === 'paper' && computerChoice === 'scissors') ||
-              (choice === 'scissors' && computerChoice === 'rock')) {
-    prompt('Computer wins!');
-  } else {
-    prompt("It's a tie");
+  switch (winner) {
+    case 'player':
+      console.log(formatResult('You win!'));
+      break;
+    case 'computer':
+      console.log(formatResult('Computer wins!'));
+      break;
+    default:
+      console.log(formatResult("It's a tie."));
   }
 }
-let answer = 'y';
 
-while (answer[0] === 'y') {
-  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-  let choice = readline.question();
+function incrementScore(winner) {
+  scores[winner] += 1;
+}
 
-  while (!VALID_CHOICES.includes(choice)) {
-    prompt("That's not a valid choice");
-    choice = readline.question();
+function displayGrandResult() {
+  consoleDivider();
+  if (scores.player > scores.computer) {
+    console.log(formatResult("Congratulations, you are the grand winner!"));
+  } else {
+    console.log(formatResult("Oops, computer is the grand winner. Better luck next time!"));
+  }
+}
+
+function promptNewGame() {
+  prompt("Do you want to play another game? (y/n)");
+  let answer = input().toLowerCase();
+
+  while (!['y', 'n'].includes(answer)) {
+    prompt("That's not a valid input. Please enter 'y' or 'n'.");
+    answer = input().toLowerCase();
+  }
+  return answer;
+}
+
+while (true) {
+
+  scores = {player: 0, computer: 0};
+
+  displayWelcome();
+
+  while ((scores.player !== REQUIRED_NUM_WINS) &&
+        (scores.computer !== REQUIRED_NUM_WINS)) {
+
+    promptContinue();
+    displayScores();
+
+    let playerChoice = getPlayerChoice();
+    let computerChoice = returnComputerChoice();
+    let winner = returnWinner(playerChoice, computerChoice);
+
+    displayRoundResult(playerChoice, computerChoice, winner);
+    incrementScore(winner);
   }
 
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
+  displayGrandResult();
 
-  displayWinner(choice, computerChoice);
-
-  prompt('Do you want to play again (y/n)?');
-  answer = readline.question().toLowerCase();
-  while (answer[0] !== 'n' && answer[0] !== 'y') {
-    
-    prompt('Please enter "y" or "n".');
-    answer = readline.question().toLowerCase();
-  }
-
+  let newGame = promptNewGame();
+  if (newGame === 'n') break;
 }
